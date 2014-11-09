e88e1538c5628f686a3f5b27a059522adc80fda8
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-11-09 11:46:25 +0800

    添加 model/artilce
    run `rails generate scaffold Article title:string body:text author:belongs_to`

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109032117) do
+ActiveRecord::Schema.define(version: 20141109034501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "articles", ["author_id"], name: "index_articles_on_author_id", using: :btree
 
   create_table "authors", force: true do |t|
     t.string   "email",                  default: "", null: false
