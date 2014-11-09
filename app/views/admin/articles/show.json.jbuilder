09d1037792f456f692acb3bfe6b5d26f6bc754a7
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-11-09 13:28:35 +0800

    Add 添加 `admin/article`

---
+++ app/views/admin/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :body, :author_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :articles
+  end
+
   resources :articles
 
   devise_for :authors
