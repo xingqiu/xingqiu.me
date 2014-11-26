d8d70e36deb25eaff15b3dcdfc38ca586c89fce8
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-11-27 00:03:53 +0800

    Bugfix config/unicorn.rb and config/unicorn_init.sh

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
-root = "/home/xingqiu/apps/xingqiu.me/current/public"
+root = "/home/xingqiu/apps/xingqiu.me/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.projectname.sock"
+listen "/tmp/unicorn.xingqiu.me.sock"
 worker_processes 2
 timeout 30
 
