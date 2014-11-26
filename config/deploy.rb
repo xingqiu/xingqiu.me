62ce7fe13b3949881f18597261f1687ccc3d52ec
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-11-26 23:24:59 +0800

    线上环境使用 shared 的 secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
