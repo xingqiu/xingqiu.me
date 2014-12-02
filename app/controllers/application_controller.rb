c0fe25c0328e381e3a011536d900c84a9c05523f
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-12-02 16:22:44 +0800

    Fix devise 使用自定义的模板

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
   def login_required
     if author_signed_in?
       return true
     end
     redirect_to "/authors/sign_in"
     return false
+  end
+
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
+
+  protected
+  def layout_by_resource
+    if devise_controller?
+      "admin"
+    else
+      "application"
+    end
   end
 end
